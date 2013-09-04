aef5a16b0c37aa7369644423d6129daa46f114cf
roshow <rolando@roshow.net>
2013-09-04 03:28:21 -0400

    using mongodb; private config.js file; cleaned up index.jade formatting

--- db_cmx.js
+++ db_cmx.js
@@ -1,7 +1,5 @@
-var db = require('mongojs').connect({
-        uri: 'cmx',
-        db: 'cmx'
-    }, ['comics']);
+var dbconfig = require('./config.js').mongo,
+    db = require('mongojs').connect(dbconfig.uri, ['comics']);
 
 exports.db_cmx = (function() {
     var db_cmx = {
